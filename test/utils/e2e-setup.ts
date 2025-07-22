592b8bf6ca97fb513a966cce9be47b017d192c3e
slogup-woodrow <woodrow@slogup.com>
2025-07-22 10:33:19 +0900

    feat:test-code

--- test/utils/e2e-setup.ts
+++ test/utils/e2e-setup.ts
@@ -6,7 +6,6 @@ dotenv.config({
   path: path.resolve(__dirname, '../envs/.env.test'),
 });
 
-// 환경변수 확인 (선택사항 - 디버깅용)
 console.log('Test environment loaded:', {
   NODE_ENV: process.env.NODE_ENV,
   DB_HOST: process.env.DB_HOST,
